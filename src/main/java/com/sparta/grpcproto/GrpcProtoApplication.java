91d57e863d8ecad34c17cb502cdfdd2126cefe64
Sun choi <csun2772@naver.com>
2024-11-15 17:29:14 +0900

    first commit

---
+++ src/main/java/com/sparta/grpcproto/GrpcProtoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.grpcproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcProtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcProtoApplication.class, args);
+    }
+
+}
